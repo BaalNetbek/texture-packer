5cd08de0f606b019e1436342d42bb3f40340b7eb
Andrey A. Ugolnik <andrey@ugolnik.info>
2017-02-09 21:13:24 +0300

    Use STB 'one-header-loader' instead libpng.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Texture Packer tool allows you to pack individual images into one texture atlas.
 ##Features##
 
 * Automatically add all images from folder or from command line.
+* Supported formats: JPEG, PNG, TGA, BMP, PSD, GIF, HDR, PIC, PNM.
 * Ability to set border around image.
 * Ability to trim input images.
 
@@ -31,9 +32,17 @@ $ git clone https://bitbucket.org/andreyu/texture-packer.git
 $ make release
 ```
 
-##Dependencies##
+##Input files notes##
 
-* libpng - png format support.
+> JPEG baseline & progressive (12 bpc/arithmetic not supported, same as stock IJG lib)
+> PNG 1/2/4/8-bit-per-channel (16 bpc not supported)
+> TGA (not sure what subset, if a subset)
+> BMP non-1bpp, non-RLE
+> PSD (composited view only, no extra channels, 8/16 bit-per-channel)
+> GIF (*comp always reports as 4-channel)
+> HDR (radiance rgbE format)
+> PIC (Softimage PIC)
+> PNM (PPM and PGM binary only)
 
 ***
 ```
--- src/imagesaver.cpp
+++ src/imagesaver.cpp
@@ -10,7 +10,8 @@
 #include "bitmap.h"
 #include "file.h"
 
-#include <png.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 
 cImageSaver::cImageSaver(const Bitmap& bitmap)
     : m_bitmap(bitmap)
@@ -23,85 +24,9 @@ cImageSaver::~cImageSaver()
 
 bool cImageSaver::save(const char* filename) const
 {
-    cFile file;
-    if (file.open(filename, "wb") == false)
-    {
-        return false;
-    }
-
-    // Create and initialize the png_struct with the desired error handler
-    // functions.  If you want to use the default stderr and longjump method,
-    // you can supply NULL for the last three parameters.  We also check that
-    // the library version is compatible with the one used at compile time,
-    // in case we are using dynamically linked libraries.  REQUIRED.
-    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-    if (png == nullptr)
-    {
-        return false;
-    }
-
-    // Allocate/initialize the image information data.  REQUIRED
-    png_infop info = png_create_info_struct(png);
-    if (info == nullptr)
-    {
-        png_destroy_write_struct(&png, (png_infopp)nullptr);
-        return false;
-    }
-
-    // Set error handling.  REQUIRED if you aren't supplying your own
-    // error hadnling functions in the png_create_write_struct() call.
-    if (setjmp(png_jmpbuf(png)))
-    {
-        png_destroy_write_struct(&png, (png_infopp)nullptr);
-        return false;
-    }
-
-    auto width = m_bitmap.width;
-    auto height = m_bitmap.height;
-
-    // set up the output control if you are using standard C streams
-    png_init_io(png, (FILE*)file.getHandle());
-
-    // Set the image information here.  Width and height are up to 2^31,
-    // bit_depth is one of 1, 2, 4, 8, or 16, but valid values also depend on
-    // the color_type selected. color_type is one of PNG_COLOR_TYPE_GRAY,
-    // PNG_COLOR_TYPE_GRAY_ALPHA, PNG_COLOR_TYPE_PALETTE, PNG_COLOR_TYPE_RGB,
-    // or PNG_COLOR_TYPE_RGB_ALPHA.  interlace is either PNG_INTERLACE_NONE or
-    // PNG_INTERLACE_ADAM7, and the compression_type and filter_type MUST
-    // currently be PNG_COMPRESSION_TYPE_BASE and PNG_FILTER_TYPE_BASE. REQUIRED
-    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB_ALPHA
-                 , PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-    //  /* Optionally write comments into the image */
-    //  text_ptr[0].key = "Title";
-    //  text_ptr[0].text = "Mona Lisa";
-    //  text_ptr[0].compression = PNG_TEXT_COMPRESSION_NONE;
-    //  text_ptr[1].key = "Author";
-    //  text_ptr[1].text = "Leonardo DaVinci";
-    //  text_ptr[1].compression = PNG_TEXT_COMPRESSION_NONE;
-    //  text_ptr[2].key = "Description";
-    //  text_ptr[2].text = "<long text>";
-    //  text_ptr[2].compression = PNG_TEXT_COMPRESSION_zTXt;
-    //  png_set_text(png, info, text_ptr, 2);
-
-    // Write the file header information.
-    png_write_info(png, info);
-
-    // Write image data
-    auto src = m_bitmap.data.data();
-    for (unsigned y = 0; y < height; y++)
-    {
-        auto row = const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(&src[y * width]));
-        png_write_row(png, row);
-    }
-
-    // It is REQUIRED to call this to finish writing the rest of the file
-    png_write_end(png, info);
-
-    png_free_data(png, info, PNG_FREE_ALL, -1);
-
-    // clean up after the write, and free any memory allocated
-    png_destroy_write_struct(&png, (png_infopp)nullptr);
-
-    return true;
+    const int w = m_bitmap.width;
+    const int h = m_bitmap.height;
+    const int stride = w * 4;
+    const auto data = m_bitmap.data.data();
+    return stbi_write_png(filename, w, h, 4, data, stride) != 0;
 }
