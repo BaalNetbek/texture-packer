ae525c43313f2145fd7d53e8b500bdf1430862bb
Alexey Egorov <a.egorov@wildspike.com>
2021-03-29 16:02:27 +0300

    Add duplication of packaed images border pixels
    To prevent blending artifacts due to interpolating image pixels with
    empty ones on the borders.

--- src/atlas/AtlasPacker.cpp
+++ src/atlas/AtlasPacker.cpp
@@ -18,6 +18,26 @@
 #include <cmath>
 #include <sstream>
 
+namespace {
+    struct PixelBlender
+    {
+        cBitmap::Pixel* start;
+
+        cBitmap::Pixel blend(size_t offsetR, size_t offsetL)
+        {
+            auto r = start + offsetR;
+            auto l = start + offsetL;
+
+            cBitmap::Pixel res;
+            res.r = r->r / 2 + l->r / 2;
+            res.g = r->g / 2 + l->g / 2;
+            res.b = r->b / 2 + l->b / 2;
+            res.a = r->a / 2 + l->a / 2;
+            return res;
+        }
+    };
+};
+
 AtlasPacker* AtlasPacker::create(size_t count, const sConfig& config)
 {
     AtlasPacker* packer = nullptr;
@@ -59,11 +79,61 @@ void AtlasPacker::copyBitmap(const sRect& rc, const cImage* image, bool overlay)
 
     for (uint32_t y = 0; y < height; y++)
     {
-        auto dst = dstData + (y + offy) * pitch + offx;
+        auto dst = dstData + (y + offy + 1) * pitch + offx + 1;
         for (uint32_t x = 0; x < width; x++)
         {
             *dst++ = *srcData++;
         }
+    }
+
+    // adding the border
+    /*
+      Consider the following picture:
+      +======+
+      |oooooo|
+      |oooooo|
+      |oooooo|
+      |oooooo|
+      +======+
+
+      o - image pixels, copied in the previous loop
+      | - left and right border
+      = - top and bottom border
+      + - corner pixels
+    */
+    auto srcLeft = bmp.getData();
+    for (uint32_t y = 0; y < height; ++y)
+    {
+        auto left = dstData + (y + offy + 1) * pitch + offx;
+        auto right = dstData + (y + offy + 1) * pitch + offx + width + 1;
+        *left = *srcLeft;
+        *right = *(srcLeft + width - 1);
+        srcLeft += width;
+    }
+
+    auto srcTop = bmp.getData();
+    for (uint32_t x = 0; x < width; ++x)
+    {
+        auto top = dstData + offy * pitch + offx + x + 1;
+        auto bottom = dstData + (offy + height + 1) * pitch + offx + x + 1;
+        *top = *srcTop;
+        *bottom = *(srcTop + width * (height - 1));
+        srcTop++;
+    }
+
+    // border corner pixels
+    {
+        auto dst = dstData + offx + offy * pitch;
+        auto borderWidth = width + 1;
+        auto borderHeight = height + 1;
+
+        PixelBlender b { dst };
+        *dst = b.blend(1, pitch); // top left
+        *(dst + borderWidth) = b.blend(width, pitch + borderWidth); // top right
+        *(dst + pitch * borderHeight) = b.blend(pitch * height, // bottom left
+                                                pitch * borderHeight + 1);
+        *(dst + pitch * borderHeight + borderWidth) = b.blend(pitch * height + borderWidth, // bottom right
+                                                              pitch * borderHeight + width);
     }
 
     if (overlay)
@@ -149,7 +219,7 @@ bool AtlasPacker::generateResFile(const char* name, const char* atlasName)
 
             out << "    ";
             out << "<" << spriteId << " texture=\"" << atlasName << "\" ";
-            out << "rect=\"" << tx << " " << ty << " " << tw << " " << th << "\" ";
+            out << "rect=\"" << tx + 1 << " " << ty + 1 << " " << tw << " " << th << "\" ";
             out << "hotspot=\"" << std::round(tw * 0.5f) << " " << std::round(th * 0.5f) << "\" />\n";
         }
         out << "</atlas>\n";
