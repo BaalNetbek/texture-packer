79860bf74482787a1a5aa9d638993f1d3a7d81ec
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-09-30 16:10:41 +0200

    feat(kdtree): optimize for thin and long sprites in both orientations

--- src/Atlas/KDTreePacker.cpp
+++ src/Atlas/KDTreePacker.cpp
@@ -31,10 +31,30 @@ bool KDTreePacker::Compare(const cImage* a, const cImage* b)
 
 #if 0
 
+    // Mode 1
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 30 packed better + 1 unchanged, 3 packed worse.
+    // The total pixel difference across all files is -3,992,040.
+
+    // ./test.sh test-wh -overlay -max 3000
+    // Out of a total of 33 files, 33 packed better + 0 unchanged, 0 packed worse.
+    // The total pixel difference across all files is -8,527,984.
+
     return (sizea.width > sizeb.height)
         || (sizea.width * sizea.height > sizeb.width * sizeb.height);
 
-#else
+#elif 0
+
+    // Mode 2
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 31 packed better + 2 unchanged, 1 packed worse.
+    // The total pixel difference across all files is -4,725,088.
+
+    // ./test.sh test-wh -overlay -max 3000
+    // Out of a total of 33 files, 30 packed better + 0 unchanged, 3 packed worse.
+    // The total pixel difference across all files is -9,487,340.
 
     auto maxa = std::max(sizea.width, sizea.height);
     auto maxb = std::max(sizeb.width, sizeb.height);
@@ -48,16 +68,6 @@ bool KDTreePacker::Compare(const cImage* a, const cImage* b)
         return false;
     }
 
-#if 0
-
-    // ./test.sh test-wh -max 3000 -overlay
-    // Out of a total of 33 files, 30 packed better + 0 unchanged, 3 packed worse.
-    // The total pixel difference across all files is -9,438,568.
-    //
-    // ./test.sh test-wz -max 3000 -overlay
-    // Out of a total of 29 files, 26 packed better + 1 unchanged, 2 packed worse.
-    // The total pixel difference across all files is -5,126,036.
-
     if (sizea.height > sizeb.height)
     {
         return true;
@@ -67,20 +77,38 @@ bool KDTreePacker::Compare(const cImage* a, const cImage* b)
         return false;
     }
 
-    return sizea.width * sizea.height > sizeb.width * sizeb.height;
+    auto areaa = sizea.width * sizea.height;
+    auto areab = sizeb.width * sizeb.height;
 
-#else
+    return areaa > areab;
 
-    // ./test.sh test-wh -max 3000 -overlay
+#elif 0
+
+    // Mode 3
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 30 packed better + 2 unchanged, 2 packed worse.
+    // The total pixel difference across all files is -4,921,888.
+
+    // ./test.sh test-wh -overlay -max 3000
     // Out of a total of 33 files, 32 packed better + 0 unchanged, 1 packed worse.
-    // The total pixel difference across all files is -8,871,488.
-    //
-    // ./test.sh test-wz -max 3000 -overlay
-    // Out of a total of 29 files, 26 packed better + 1 unchanged, 2 packed worse.
-    // The total pixel difference across all files is -5,372,264.
+    // The total pixel difference across all files is -8,932,660.
+
+    auto maxa = std::max(sizea.width, sizea.height);
+    auto maxb = std::max(sizeb.width, sizeb.height);
+
+    if (maxa > maxb)
+    {
+        return true;
+    }
+    if (maxb < maxa)
+    {
+        return false;
+    }
 
     auto areaa = sizea.width * sizea.height;
     auto areab = sizeb.width * sizeb.height;
+
     if (areaa > areab)
     {
         return true;
@@ -92,7 +120,81 @@ bool KDTreePacker::Compare(const cImage* a, const cImage* b)
 
     return sizea.height > sizeb.height;
 
-#endif
+#elif 0
+
+    // Mode 4
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 28 packed better + 2 unchanged, 4 packed worse.
+    // The total pixel difference across all files is -4,127,476.
+
+    // ./test.sh test-wh -overlay -max 3000
+    // Out of a total of 33 files, 29 packed better + 0 unchanged, 4 packed worse.
+    // The total pixel difference across all files is -8,066,584.
+
+    if (sizea.width > sizeb.width)
+    {
+        return true;
+    }
+    if (sizea.width < sizeb.width)
+    {
+        return false;
+    }
+
+    auto areaa = sizea.width * sizea.height;
+    auto areab = sizeb.width * sizeb.height;
+
+    return areaa > areab;
+
+#elif 0
+
+    // Mode 5
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 30 packed better + 2 unchanged, 2 packed worse.
+    // The total pixel difference across all files is -4,921,888.
+
+    // ./test.sh test-wh -overlay -max 3000
+    // Out of a total of 33 files, 32 packed better + 0 unchanged, 1 packed worse.
+    // The total pixel difference across all files is -8,943,940.
+
+    auto maxa = std::max(sizea.width, sizea.height);
+    auto maxb = std::max(sizeb.width, sizeb.height);
+
+    if (maxa > maxb)
+    {
+        return true;
+    }
+
+    auto areaa = sizea.width * sizea.height;
+    auto areab = sizeb.width * sizeb.height;
+
+    if (areaa > areab)
+    {
+        return true;
+    }
+
+    return maxb < maxa;
+
+#else
+
+    // Mode 6
+    // This algorithm is optimized for handling thin and long sprites,
+    // both vertical and horizontal, ensuring efficient processing
+    // regardless of their orientation or aspect ratio.
+
+    // ./test.sh test-wz -overlay -max 3000
+    // Out of a total of 34 files, 29 packed better + 2 unchanged, 3 packed worse.
+    // The total pixel difference across all files is -4,673,840.
+
+    // ./test.sh test-wh -overlay -max 3000
+    // Out of a total of 33 files, 30 packed better + 0 unchanged, 3 packed worse.
+    // The total pixel difference across all files is -9,441,708.
+
+    auto maxa = std::max(sizea.width, sizea.height);
+    auto maxb = std::max(sizeb.width, sizeb.height);
+
+    return maxa > maxb;
 
 #endif
 }
