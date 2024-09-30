1e79f05f2870ddc6a04dc5db699b46304b5b25ea
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-09-30 14:57:33 +0200

    refact(KDTree): move cKDNode to separate file

---
+++ src/Atlas/KDNode.cpp
@@ -0,0 +1,102 @@
+/**********************************************\
+*
+*  Andrey A. Ugolnik
+*  http://www.ugolnik.info
+*  andrey@ugolnik.info
+*
+\**********************************************/
+
+#include "KDNode.h"
+
+cKDNode::cKDNode(const sRect& area, uint32_t padding)
+    : m_area(area)
+    , m_padding(padding)
+{
+}
+
+cKDNode::~cKDNode()
+{
+    delete m_childA;
+    delete m_childB;
+}
+
+cKDNode* cKDNode::add(const sSize& size)
+{
+    if (isLeaf())
+    {
+        // end of the tree, no more room
+        if (m_used)
+        {
+            return nullptr;
+        }
+
+        const auto padding = m_padding;
+
+        const auto imgWidth = size.width + padding * 2;
+        const auto imgHeight = size.height + padding * 2;
+
+        const auto nodeWidth = m_area.width();
+        const auto nodeHeight = m_area.height();
+
+        const auto x = m_area.left;
+        const auto y = m_area.top;
+        m_rect = { x, y, x + size.width, y + size.height };
+
+        // size matches exactly
+        if (imgWidth == nodeWidth && imgHeight == nodeHeight)
+        {
+            m_used = true;
+            return this;
+        }
+
+        // rect is too big for this node
+        if (imgWidth > nodeWidth || imgHeight > nodeHeight)
+        {
+            return nullptr;
+        }
+
+        // split this node in two
+        const auto subwidth = nodeWidth - imgWidth;
+        const auto subheight = nodeHeight - imgHeight;
+
+        // static int last = -1;
+        if (subwidth <= subheight)
+        {
+            // if (last != 0)
+            // {
+            // last = 0;
+            // printf("-"); fflush(nullptr);
+            // }
+            // split --
+            m_childA = new cKDNode({ x, y, x + nodeWidth, y + imgHeight }, padding);
+            m_childB = new cKDNode({ x, y + imgHeight, x + nodeWidth, y + imgHeight + subheight }, padding);
+        }
+        else
+        {
+            // if (last != 1)
+            // {
+            // last = 1;
+            // printf("|"); fflush(nullptr);
+            // }
+            // split |
+            m_childA = new cKDNode({ x, y, x + imgWidth, y + nodeHeight }, padding);
+            m_childB = new cKDNode({ x + imgWidth, y, x + imgWidth + subwidth, y + nodeHeight }, padding);
+        }
+
+        return m_childA->add(size);
+    }
+    else if (m_childA != nullptr)
+    {
+        auto node = m_childA->add(size);
+        if (node != nullptr)
+        {
+            return node;
+        }
+        else if (m_childB != nullptr)
+        {
+            return m_childB->add(size);
+        }
+    }
+
+    return nullptr;
+}
---
+++ src/Atlas/KDNode.h
@@ -0,0 +1,42 @@
+/**********************************************\
+*
+*  Andrey A. Ugolnik
+*  http://www.ugolnik.info
+*  andrey@ugolnik.info
+*
+\**********************************************/
+
+#pragma once
+
+#include "Types/Types.h"
+
+class cKDNode final
+{
+public:
+    cKDNode(const sRect& area, uint32_t padding);
+    ~cKDNode();
+
+    cKDNode* add(const sSize& size);
+
+    const sRect& getRect() const
+    {
+        return m_rect;
+    }
+
+private:
+    bool isLeaf() const
+    {
+        return m_childA == nullptr || m_childB == nullptr;
+    }
+
+private:
+    const sRect m_area;
+    const uint32_t m_padding;
+
+private:
+    bool m_used = false;
+    cKDNode* m_childA = nullptr; // left or top
+    cKDNode* m_childB = nullptr; // right or bottom
+
+    sRect m_rect;
+};
